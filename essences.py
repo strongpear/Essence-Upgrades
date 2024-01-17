19b3e7ad13915276f44b0799857ae483dcb34f8b
strongpear <jlee4804@gmail.com>
2024-01-17 13:25:05 -0600

    add prices

--- essences.py
+++ essences.py
@@ -5,7 +5,12 @@ import math
 
 
 response = requests.get('https://poe.ninja/api/data/itemoverview?league=Affliction&type=Essence')
-data = response.json()
+
+if response.status_code == 200:
+    # Process the response content here
+    data = response.json()
+else:
+    print(f"Request failed with status code: {response.status_code}")
 df = pd.json_normalize(data, 'lines')
 screamingDF = df.loc[df.name.str.contains('Screaming')]
 shriekingDF = df.loc[df.name.str.contains('Shrieking')]
@@ -15,14 +20,13 @@ types = ['Greed', 'Contempt', 'Hatred', 'Woe', 'Fear', 'Anger', 'Torment', 'Sorr
 prices = {}
 upgrade = {}
 essences = [shriekingDF, deafeningDF]
-
 for type in types:
     temp = []
     for essence in essences:
         essenceValue = essence[essence.name.str.contains(type)]['chaosValue'].values[0]
         temp.append(essenceValue)
     prices[type] = temp
-
+print(prices)
 for key in prices:
     if math.floor(prices[key][1]) > 3 * prices[key][0]:
         upgrade[key] = "YES"
@@ -37,22 +41,24 @@ server = app.server
 half = len(upgrade) // 2 + 2
 leftSide = dict(list(upgrade.items())[:half])
 rightSide = dict(list(upgrade.items())[half:])
+leftPrice = dict(list(prices.items())[:half])
+rightPrice = dict(list(prices.items())[half:])
 app.layout = html.Div([
     html.Table([
-        html.Tr([html.Th("Key"), html.Th("Upgrade")]),
+        html.Tr([html.Th("Key"), html.Th("Upgrade"), html.Th("Shrieking"), html.Th("Deafening")]),
         *[
-            html.Tr([html.Td(key), html.Td(str(value))])
-            for key, value in leftSide.items()
+            html.Tr([html.Td(key), html.Td(str(value)), html.Td(leftPrice[price][0]), html.Td(str(leftPrice[price][1]))])
+            for key, value, price in zip(leftSide.keys(), leftSide.values(), leftPrice)
         ]
-    ], style={'display': 'inline-block', 'margin': 'auto', 'width': '20%', 'vertical-align': 'top'}),
+    ], style={'display': 'inline-block', 'width': '20%', 'vertical-align': 'top'}),
 
     html.Table([
-        html.Tr([html.Th("Key"), html.Th("Upgrade")]),
+        html.Tr([html.Th("Key"), html.Th("Upgrade"), html.Th("Shrieking"), html.Th("Deafening")]),
         *[
-            html.Tr([html.Td(key), html.Td(str(value))])
-            for key, value in rightSide.items()
+            html.Tr([html.Td(key), html.Td(str(value)), html.Td(rightPrice[price][0]), html.Td(str(rightPrice[price][1]))])
+            for key, value, price in zip(rightSide.keys(), rightSide.values(), rightPrice)
         ]
-    ], style={'display': 'inline-block', 'margin': 'auto', 'width': '20%', 'vertical-align': 'top'})
+    ], style={'display': 'inline-block', 'width': '20%', 'vertical-align': 'top'}),
 ], style={'text-align': 'center'})
 
 # Run server
